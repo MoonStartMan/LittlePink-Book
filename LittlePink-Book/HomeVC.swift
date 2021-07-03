0577bfa29c7e82f9f4b40af8d4d26703164169f8
MoonStarMan <974556594@qq.com>
2021-07-03 16:07:42 +0800

    APPicon、启动页。

--- LittlePink-Book/HomeVC.swift
+++ LittlePink-Book/HomeVC.swift
@@ -1,19 +1,19 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  LittlePink-Book
 //
 //  Created by 王潇 on 2021/7/3.
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class ViewController: UIViewController {
+class HomeVC: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
